6fc5c1920b073db07e2f06f10be7afc2213e8a69
Fabian Giesche <FGiesche@apptech.de>
2024-05-14 13:23:12 +0200

    add datagrid to workspace component

--- at.PowerBIUnitTest.Portal/at.PowerBIUnitTest.Portal/ClientApp/src/app/components/unit-tests/unit-tests.component.ts
+++ at.PowerBIUnitTest.Portal/at.PowerBIUnitTest.Portal/ClientApp/src/app/components/unit-tests/unit-tests.component.ts
@@ -23,7 +23,7 @@ import { TabularModelService } from "src/app/shared/services/tabular-model.servi
     templateUrl: "./unit-tests.component.html",
     styleUrls: ["./unit-tests.component.css"],
 })
-export class UnitTestsComponent implements OnInit {
+export class UnitTestsComponent {
     public dataSourceWorkspaces: DataSource;
     public dataSourceWorkspacesOdata: DataSource;
     public dataSourceTabularModels: DataSource;
@@ -54,7 +54,6 @@ export class UnitTestsComponent implements OnInit {
     ) {
         this.onClickExecuteUnitTests = this.onClickExecuteUnitTests.bind(this);
         this.onClickAddUserStory = this.onClickAddUserStory.bind(this);
-        this.onClickDeleteWorkspace = this.onClickDeleteWorkspace.bind(this);
         this.onClickDeleteTabularModel = this.onClickDeleteTabularModel.bind(this);
         this.onClickEditUserStory = this.onClickEditUserStory.bind(this);
         this.onClickDeleteUserStory = this.onClickDeleteUserStory.bind(this);
@@ -103,28 +102,6 @@ export class UnitTestsComponent implements OnInit {
             store: this.workspaceService.getStore(),
             sort: [{ selector: "Name", desc: false }]
         });
-    }
-
-    ngOnInit(): void { }
-
-    public onClickPullWorkspaces(e: ClickEvent): void {
-        this.layoutService.change(LayoutParameter.ShowLoading, true);
-        this.workspaceService
-            .pullWorkspaces()
-            .then(() => {
-                this.treeList.instance.refresh();
-                this.layoutService.notify({
-                    message: "Workspaces pulled successfully",
-                    type: NotificationType.Success,
-                });
-            })
-            .catch((error: Error) => this.layoutService.notify({
-                message: error.message ? `Can not pull workspaces: ${error.message}` : "Error while pulling workspaces",
-                type: NotificationType.Error,
-            }))
-            .finally(() =>
-                this.layoutService.change(LayoutParameter.ShowLoading, false)
-            );
     }
 
     public onToolbarPreparingTreeList(e: ToolbarPreparingEvent): void {
@@ -178,18 +155,6 @@ export class UnitTestsComponent implements OnInit {
                 onClick: this.onClickExecuteMultipleUnitTests.bind(this),
             },
             location: "after",
-        });
-
-        toolbarItems.unshift({
-            widget: "dxButton",
-            options: {
-                icon: "download",
-                text: "Pull Workspaces",
-                stylingMode: "contained",
-                type: "normal",
-                onClick: this.onClickPullWorkspaces.bind(this),
-            },
-            location: "before",
         });
     }
 
@@ -314,28 +279,6 @@ export class UnitTestsComponent implements OnInit {
 
     public onClickEditUserStory(e: any): void {
         this.openEditUserStoryPopup(e.row.data);
-    }
-
-    public onClickDeleteWorkspace(e: any): void {
-        let result = confirm("Are you sure you want to delete this workspace (including all tabular models, user stories and unit tests)? If it continues to exist in Power BI, it will be created again with the next pull action.", "Delete Workspace");
-        result.then((dialogResult) => {
-            if (dialogResult) {
-                this.layoutService.change(LayoutParameter.ShowLoading, true);
-                this.workspaceService.remove(e.row.data.Id)
-                    .then(() => this.layoutService.notify({
-                        type: NotificationType.Success,
-                        message: "The workspace was deleted successfully."
-                    }))
-                    .catch((error: Error) => this.layoutService.notify({
-                        type: NotificationType.Error,
-                        message: error?.message ? `The workspace could not be deleted: ${error.message}` : "The workspace could not be deleted."
-                    }))
-                    .then(() => {
-                        this.treeList.instance.refresh();
-                        this.layoutService.change(LayoutParameter.ShowLoading, false);
-                    });
-            }
-        });
     }
 
     public onClickDeleteTabularModel(e: any): void {
--- at.PowerBIUnitTest.Portal/at.PowerBIUnitTest.Portal/ClientApp/src/app/components/workspaces/workspaces.component.ts
+++ at.PowerBIUnitTest.Portal/at.PowerBIUnitTest.Portal/ClientApp/src/app/components/workspaces/workspaces.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DxDataGridComponent } from 'devextreme-angular';
+import DataSource from 'devextreme/data/data_source';
+import { ClickEvent } from 'devextreme/ui/button';
+import { ToolbarPreparingEvent } from 'devextreme/ui/data_grid';
+import { LayoutParameter, LayoutService, NotificationType } from 'src/app/shared/services/layout.service';
+import { WorkspaceService } from 'src/app/shared/services/workspace.service';
 
 @Component({
     selector: 'app-workspaces',
@@ -7,5 +13,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkspacesComponent {
 
-    constructor() { }
+    public dataSourceWorkspaces: DataSource;
+
+    @ViewChild(DxDataGridComponent, { static: false })
+    dataGrid: DxDataGridComponent;
+
+    constructor(private workspaceService: WorkspaceService, private layoutService: LayoutService) {
+        this.dataSourceWorkspaces = new DataSource({
+            store: workspaceService.getStore()
+        });
+    }
+
+    public onToolbarPreparingTreeList(e: ToolbarPreparingEvent): void {
+        let toolbarItems = e.toolbarOptions.items;
+
+        toolbarItems.unshift({
+            widget: "dxButton",
+            options: {
+                icon: "refresh",
+                stylingMode: "contained",
+                type: "normal",
+                hint: "Refresh Applications",
+                onClick: this.onClickRefresh.bind(this),
+            },
+            location: "after",
+        });
+
+        toolbarItems.unshift({
+            widget: "dxButton",
+            options: {
+                icon: "download",
+                text: "Pull Workspaces",
+                stylingMode: "contained",
+                type: "normal",
+                onClick: this.onClickPullWorkspaces.bind(this),
+            },
+            location: "before",
+        });
+    }
+
+    
+    public onClickPullWorkspaces(e: ClickEvent): void {
+        this.layoutService.change(LayoutParameter.ShowLoading, true);
+        this.workspaceService
+            .pullWorkspaces()
+            .then(() => {
+                this.dataGrid.instance.refresh();
+                this.layoutService.notify({
+                    message: "Workspaces pulled successfully",
+                    type: NotificationType.Success,
+                });
+            })
+            .catch((error: Error) => this.layoutService.notify({
+                message: error.message ? `Can not pull workspaces: ${error.message}` : "Error while pulling workspaces",
+                type: NotificationType.Error,
+            }))
+            .finally(() =>
+                this.layoutService.change(LayoutParameter.ShowLoading, false)
+            );
+    }
+
+    public onClickRefresh(e: ClickEvent): void {
+        this.dataGrid.instance.refresh();
+    }
 }
