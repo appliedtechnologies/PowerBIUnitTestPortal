f32ec411c02d8497d5861f5bb359dfd61d8625a4
Fabian Giesche <FGiesche@apptech.de>
2024-05-14 13:08:58 +0200

    add empty workspaces component

--- at.PowerBIUnitTest.Portal/at.PowerBIUnitTest.Portal/ClientApp/src/app/app-routing.module.ts
+++ at.PowerBIUnitTest.Portal/at.PowerBIUnitTest.Portal/ClientApp/src/app/app-routing.module.ts
@@ -7,10 +7,12 @@ import { RoleGuard } from "./shared/guards/role.guard";
 import { UnitTestsComponent } from "./components/unit-tests/unit-tests.component";
 import { MsalGuard } from "@azure/msal-angular";
 import { HistoryComponent } from "./components/history/history.component";
+import { WorkspacesComponent } from "./components/workspaces/workspaces.component";
 
 export const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "/unittests" },
   { path: "profile", component: ProfileComponent, canActivate: [MsalGuard] },
   { path: "unittests", component: UnitTestsComponent, canActivate: [MsalGuard] },
   { path: "history", component: HistoryComponent, canActivate: [MsalGuard] },
+  { path: "workspaces", component: WorkspacesComponent, canActivate: [MsalGuard] },
 ];
---
+++ at.PowerBIUnitTest.Portal/at.PowerBIUnitTest.Portal/ClientApp/src/app/components/workspaces/workspaces.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-workspaces',
+    templateUrl: './workspaces.component.html',
+    styleUrls: ['./workspaces.component.css']
+})
+export class WorkspacesComponent {
+
+    constructor() { }
+}
